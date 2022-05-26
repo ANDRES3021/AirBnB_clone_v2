d6d9935cde6a55e5ccb068e6563a5d9207666c8e
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-25 20:41:07 -0500

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application listening 0.0.0.0 port 5000:
-    The route of the domain / displays 'Hello HBNB!'
-"""
+""" Script that starts a Flask web application """
 
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
